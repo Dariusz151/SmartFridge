16eb1c395f2add49b612a8fd56c20c9ad16cdb34
Dariusz <dariusz151@gmail.com>
2018-09-06 23:48:24 +0200

    ChooseRecipe function returns array of numbers; added test recipes

---
+++ InsertArticles.sql
@@ -0,0 +1,29 @@
+USE FridgeDB
+GO
+
+	INSERT INTO [dbo].[Articles]
+		([ArticleName], [Quantity], [Weight])
+	VALUES 
+		( 'Kurczak', 1,  200),
+		( 'Ry¿', 1,  100),
+		( 'Cebula', 1,  50),
+		( 'Quinoa', 1,  100),
+		( 'Ziemniaki', 5,  50),
+		( 'Frytki', 1,  700),
+		( 'Sa³ata', 1,  100),
+		( 'Kiwi', 3,  50),
+		( 'Banan', 3,  100),
+		( 'Gruszka', 5,  70),
+		( 'Miêso wo³owe', 1,  500),
+		( 'Bu³ka hamburgerowa', 2,  200),
+		( 'Pomidor', 1,  100),
+		( 'Nachos', 50,  10),
+		( 'Rodzynki', 1,  100),
+		( 'Ogórek', 3,  200),
+		( 'Ser feta', 1,  250),
+		( 'Kukurydza', 1,  250),
+		( 'Makaron', 1,  400),
+		( 'Miruna', 1,  400),
+		( 'Tofu', 1,  100),
+		( 'Szpinak', 10,  200),
+		( 'Mleko', 2,  200)
--- SmartFridge/wwwroot/js/ChooseRecipe.js
+++ SmartFridge/wwwroot/js/ChooseRecipe.js
@@ -1,4 +1,8 @@
-﻿
+﻿var recipes = [];
+
+$(document).ready(function () {
+    GetAllRecipes();
+});
 
 function ChooseRecipe()
 {
@@ -11,11 +15,35 @@ function ChooseRecipe()
         selectedItems[index] = articlesTable[number].id;
     });
     var selectedItems_names = [];
-    console.log(selectedItems);
+
     selectedItems.forEach(function (value, index) {
-        selectedItems_names[index] = articlesTable[index].articleName;
+        selectedItems_names[index] = articlesTable[articlesTable.findIndex(x => x.id === value)].articleName;
     });
+    
+    var dinnerNumbers = WhichRecipe(selectedItems_names, recipes);
 
+    console.log(dinnerNumbers);
+}
 
+function ContainsAll(array1, array2) {
+    return array1.every(elem => array2.indexOf(elem) > -1)
+}
 
+function GetAllRecipes() {
+    for (var k = 0; k < 16; k++) {
+        $.getJSON("/recipes/dinners/dinner" + (k + 1) + ".json", function (json) {
+            recipes.push(json);
+        });
+    }
+}
+
+function WhichRecipe(selectedItems_names, recipes) {
+    var dinnerNumbers = [];
+
+    for (var k = 0; k < recipes.length; k++) {
+        if (ContainsAll(selectedItems_names, recipes[k].components))
+            dinnerNumbers.push(k);
+    }
+    
+    return dinnerNumbers;
 }
